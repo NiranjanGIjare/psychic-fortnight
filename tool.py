51bba52270bf4ce758b8c2ee88d4dac88041b9b0
NiranjanGIjare <32029288+NiranjanGIjare@users.noreply.github.com>
2018-06-27 16:38:17 +0530

    Update tool.py

--- tool.py
+++ tool.py
@@ -6,12 +6,7 @@ import sys
 if len(sys.argv)==1:
     os.system("firefox https://www.google.com")
 else:
-    l=sys.argv[2::]
-    search_string=sys.argv[1]
-    for argument in l:
-        search_string=search_string+"+"+argument
-    search_string="https://www.google.com/search?q="+search_string+"&ie=utf-8&oe=utf-8&client=firefox-b-ab"
     f=open("/home/fedora/.google-command-line-tool/search.txt",'w')
-    f.write(search_string)
+    f.write("https://www.google.com/search?q="+'+'.join(sys.argv[1::])+"&ie=utf-8&oe=utf-8&client=firefox-b-ab")
     f.close()
     os.system("firefox $(cat /home/fedora/.google-command-line-tool/search.txt)")
