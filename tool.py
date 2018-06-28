79af8a2dfb042f84d27cac21c680db20d1a47362
NiranjanGIjare <32029288+NiranjanGIjare@users.noreply.github.com>
2018-06-28 20:56:08 +0530

    Update tool.py

--- tool.py
+++ tool.py
@@ -3,10 +3,20 @@
 import os
 import sys
 
+def allsearch(strt):
+    f=open("/tmp/search.txt",'w')
+    f.write("https://www.google.com/search?q="+'+'.join(sys.argv[strt::])+"&ie=utf-8&oe=utf-8&client=firefox-b-ab")
+    f.close()
+    os.system("firefox $(cat /tmp/search.txt)")
+
 if len(sys.argv)==1:
     os.system("firefox https://www.google.com")
-else:
-    f=open("/home/fedora/.google-command-line-tool/search.txt",'w')
-    f.write("https://www.google.com/search?q="+'+'.join(sys.argv[1::])+"&ie=utf-8&oe=utf-8&client=firefox-b-ab")
+elif sys.argv[1]=='-i':
+    f=open("/tmp/search.txt",'w')
+    f.write("https://www.google.co.in/search?hl=en&tbm=isch&source=hp&biw=1366&bih=630&ei=Sfo0W4DFHs2c9QO0gIugCg&q="+'+'.join(sys.argv[2::])+"&oq="+'+'.join(sys.argv[2::])+"&gs_l=img.3..0l10.")
     f.close()
-    os.system("firefox $(cat /home/fedora/.google-command-line-tool/search.txt)")
+    os.system("firefox $(cat /tmp/search.txt)")
+elif sys.argv[1]=='-a':
+    allsearch(2)
+else :
+    allsearch(1)
